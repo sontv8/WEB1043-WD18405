16c3181716fdfd686aa691c626af8d9a8d172506
sontv8 <sontv8@fpt.edu.vn>
2023-07-15 17:47:00 +0700

    day5 - thực hành

--- Day05/main.js
+++ Day05/main.js
@@ -5,24 +5,29 @@
 
 // console.log(document.querySelectorAll('h2'));
 
-var mainTitle = document.querySelector('#main-title')
-// mainTitle.innerHTML = 'Helo cac ban';
-// mainTitle.id = 'abcd';
-mainTitle.className = 'abc';
-mainTitle.style.border = '5px dashed orange';
-mainTitle.style.fontSize = '50px';
+// var mainTitle = document.querySelector('#main-title')
+// // mainTitle.innerHTML = 'Helo cac ban';
+// // mainTitle.id = 'abcd';
+// mainTitle.className = 'abc';
+// mainTitle.style.border = '5px dashed orange';
+// mainTitle.style.fontSize = '50px';
+
 /*
     thiết lập chiều rộng cho khối div có class box là 300px
     thiết lập chiều cao cho khối div có class box là 300px
     thiết lập background color là màu xanh
-
     cập nhật nội dung hiển thị bên trong khối div là "My box"
     thay đổi class thành "red-box"
     thêm id cho khối div là "red-box"
-
-
 */
 
+// var box = document.querySelector('.box')
+// box.style.width = '300px';
+// box.style.height = '300px';
+// box.style.backgroundColor = 'blue';
+// box.innerHTML = 'My box';
+// box.className = 'red-box';
+// box.id = 'red-box'
 /*
     <div id="product">
         <h3 id="product-name"></h3>
@@ -36,3 +41,12 @@ var product = {
     brand: 'Apple',
     price: '799$'
 }
+
+var productName = document.querySelector('#product-name')
+var productBrand = document.querySelector('#product-brand')
+var productPrice = document.querySelector('#product-price')
+
+console.log(product.name);
+productName.innerHTML = product.name
+productBrand.innerHTML = product.brand
+productPrice.innerHTML = product.price
