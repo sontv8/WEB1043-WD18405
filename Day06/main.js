251e70a0be7232252a6d4069ee35469dc451b448
sontv8 <sontv8@fpt.edu.vn>
2023-07-22 17:19:32 +0700

    day08-demo asm1

--- Day06/main.js
+++ Day06/main.js
@@ -6,11 +6,27 @@ var menus = [
     { id: 5, name: 'About', path: '/about' }
 ]
 
-var ul = document.querySelector("ul")
+// var ul = document.querySelector("ul")
 
-for (let item of menus) {
-    console.log(item.name);
-    ul.innerHTML += `
-        <li><a href="${item.path}">${item.name}</a></li>
+// for (let item of menus) {
+//     console.log(item.name);
+//     ul.innerHTML += `
+//         <li><a href="${item.path}">${item.name}</a></li>
+//     `
+// }
+const navBar = document.querySelector(".nav-bar")
+navBar.innerHTML = menuRender();
+
+function menuRender() {
+
+    return `
+        <nav>
+            <ul>
+            ${menus.map((item) => {
+        return `<li><a href="${item.path}">${item.name}</a></li>`
+    }).join("")}
+            </ul>
+        </nav>
     `
+
 }
